352b4d014bd0f8480e8ba18776bdb43043cc7618
SabonaWaktole <sebonawaktole@gmail.com>
2025-07-18 03:33:48 +0300

    implement home page and add top box for detail page

---
+++ mobile_sabona/lib/datas/product.dart
@@ -0,0 +1,25 @@
+// product.dart
+
+class Product {
+
+
+    final String name;
+    final String description;
+    final double price;
+    final String imagePath;
+    final String category;
+    final double rating;
+
+
+    
+
+Product({
+    required this.name,
+    required this.category,
+    required this.price,
+    required this.imagePath,
+    required this.description,
+    required this.rating,
+
+});
+}
